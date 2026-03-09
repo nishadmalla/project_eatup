a74495b770ba8f8e92f52b3e3e3338906db62ffc
nishad0412 <149762693+nishadmalla@users.noreply.github.com>
2026-03-09 22:43:29 +0545

    model

---
+++ lib/features/auth/data/models/auth_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class AuthModelAdapter extends TypeAdapter<AuthModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  AuthModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return AuthModel(
+      userId: fields[0] as String?,
+      name: fields[1] as String,
+      email: fields[2] as String,
+      password: fields[3] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, AuthModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.userId)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.email)
+      ..writeByte(3)
+      ..write(obj.password);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AuthModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
